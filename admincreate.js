1dc82e917dd6e46d776b017673a5db2a8644dcde
Rachel Goldstein <Rachel@alexs-MacBook-Pro-2.local>
2018-10-27 14:57:54 -0700

    modifying admincreate

---
+++ admincreate.js
@@ -0,0 +1,13 @@
+//functions so when user submits form the value they typed associated with id
+// have function console.log() (print) to screen for now
+
+function printForm(){
+  console.log(Organizer);
+  console.log(Phone);
+  console.log(Date);
+  console.log(Location);
+  console.log(Email);
+  console.log(Event Title);
+  console.log(Time);
+  console.log(Description);
+}
