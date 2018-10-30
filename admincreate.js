f53fa1b5a0892948992a371f8e96e37757c499e7
arshij <arshi.jujara98@gmail.com>
2018-10-30 15:46:10 -0700

    Ajax tings

--- admincreate.js
+++ admincreate.js
@@ -1,13 +1,25 @@
-//functions so when user submits form the value they typed associated with id
-// have function console.log() (print) to screen for now
-
-function printForm(){
-  console.log(Organizer);
-  console.log(Phone);
-  console.log(Date);
-  console.log(Location);
-  console.log(Email);
-  console.log(Event Title);
-  console.log(Time);
-  console.log(Description);
-}
+$("#addevent").click(function(){
+    var date = $( "#date" ).val();
+    var time = $( "#time" ).val();
+    var title = $( "#title" ).val();
+    var description = $( "#description" ).val();
+    var location = $( "#location" ).val();
+    var postedby = $( "#postedby" ).val();
+    var email = $( "#email" ).val();
+    $.ajax({
+      url: "ajax.aspx",
+      type: "post", //send it through get method
+      data: { 
+        eventdate: date,
+        eventtime: time,
+        eventtitle: title,
+        eventdescription: description,
+        eventlocation: location,
+        eventorganizer: postedby,
+        eventemail: email
+      },
+      success: function(response) {
+        console.log("Success");
+      }
+    });
+});
