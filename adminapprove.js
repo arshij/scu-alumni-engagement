a8a95fdcd539081e8a09d646105a1bb34b8fa625
arshij <arshi.jujara98@gmail.com>
2018-11-11 18:47:59 -0800

    Fixed admin approve, added cookies, worked on login

--- adminapprove.js
+++ adminapprove.js
@@ -1,61 +1,44 @@
 // Hardcoded data for initial system demo
 var myEvents = [
 {
-    "date" : "11/02/2018",
-    "time" : "12:00pm PDT",
-    "name" : "First Friday Mass and Lunch",
-    "description" : "Join us for noon Mass in the Mission Church followed by a catered lunch in Donohoe Alumni House.",
-    "location" : "Mission Church",
-    "postedby" : "SCU Alumni Office"
+    "EVENTDATE" : "11/02/2018",
+    "EVENTTIME" : "12:00pm PDT",
+    "EVENTNAME" : "First Friday Mass and Lunch",
+    "EVENTDESCRIPTION" : "Join us for noon Mass in the Mission Church followed by a catered lunch in Donohoe Alumni House.",
+    "EVENTLOCATION" : "Mission Church",
+    "EVENTPOSTEDBY" : "SCU Alumni Office"
 },
 {
-    "date" : "11/07/2018",
-    "time" : "6:15pm PST",
-    "name" : "Come Meet Jeff Miller ’73 and the Larry O’Brien NBA Championship Trophies",
-    "description" : "Join us as we welcome back Jeff Miller ’73, MBA ’76 to Santa Clara. Listen to this Bronco talk about life after SCU and winning three NBA championship trophies on the executive board of the Warriors organization. After take a photo with the 2015, 2017, and 2018 trophies and meet Jeff.",
-    "location" : "St. Clare Room in SCU Library",
-    "postedby" : "SCU Alumni Office"
+    "EVENTDATE" : "11/07/2018",
+    "EVENTTIME" : "6:15pm PST",
+    "EVENTNAME" : "Come Meet Jeff Miller ’73 and the Larry O’Brien NBA Championship Trophies",
+    "EVENTDESCRIPTION" : "Join us as we welcome back Jeff Miller ’73, MBA ’76 to Santa Clara. Listen to this Bronco talk about life after SCU and winning three NBA championship trophies on the executive board of the Warriors organization. After take a photo with the 2015, 2017, and 2018 trophies and meet Jeff.",
+    "EVENTLOCATION" : "St. Clare Room in SCU Library",
+    "EVENTPOSTEDBY" : "SCU Alumni Office"
 }
 ];
 
-// Appending data to HTML table
-var tbody = document.getElementById('tbody');
-var title = document.getElementById('regTitle');
+var imported = document.createElement('script');
+imported.src = 'api.js';
+document.head.appendChild(imported);
 
-// change buttons to be approve or deny events
-for (var i = 0; i < myEvents.length; i++) {
-    var tr = '<tr>';
-    //weird spacing of buttons??
-    tr += "<td>" + myEvents[i].date + "</td>" + "<td>" + myEvents[i].time + "</td>" + "<td class='eventname' value=" + "'" + myEvents[i].name + "'>" + myEvents[i].name + "</td>" + "<td>" + myEvents[i].description + "</td>" + "<td>" + myEvents[i].location + "</td>" + "<td>" + myEvents[i].postedby + "</td>" + "<td>" + '<button type="button" class="btn btn-info btn-sm" style="margin-top:30%;" id=' + i + ' data-target="#regmodal"; onclick="approve(' + i + ')" data-toggle="modal";>Approve</button>' + "</td>" + "<td>" + '<button type="button" class="btn btn-info btn-sm" style="margin-top:30%;" id=' + i + ' data-target="#regmodal"; onclick="reject(' + i + ')" data-toggle="modal";>Reject</button>' + "</td>" + "</tr>" + "</tr>";
-    tbody.innerHTML += tr;
-}
+$(document).ready(function(){
+    //var myEvents = viewEvents();
+    // Appending data to HTML table
+    var tbody = document.getElementById('tbody');
+    var title = document.getElementById('regTitle');
 
-$("#approvebutton").click(function() {
-        var eventname = window.sessionStorage.getItem('eventname');
-        var firstname = $( "#firstname" ).val();
-        var lastname = $( "#lastname" ).val();
-        var registeremail = $( "#registeremail" ).val();
-        var gradyear = $( "#gradyear" ).val();
-        var guests = $( "#guests" ).val();
-        console.log(firstname + lastname + registeremail + gradyear + guests + eventname);
+    for (var i = 0; i < myEvents.length; i++) {
+        var tr = '<tr>';
+        tr += "<td>" + myEvents[i].EVENTDATE + "</td>" + "<td>" + myEvents[i].EVENTTIME + "</td>" + "<td class='eventname' value=" + "'" + myEvents[i].EVENTNAME + "'>" + myEvents[i].EVENTNAME + "</td>" + "<td>" + myEvents[i].EVENTDESCRIPTION + "</td>" + "<td>" + myEvents[i].EVENTLOCATION + "</td>" + "<td>" + myEvents[i].EVENTPOSTEDBY + "</td>" + "<td>" + '<button type="button" class="btn btn-info btn-sm" style="margin-top:30%;" id=' + i + ' onclick="approve(' + "'" + myEvents[i].EVENTNAME + "'" + ')">Approve</button>' + "</td>" + "<td>" + '<button type="button" class="btn btn-info btn-sm" style="margin-top:36%;" id=' + i + '; onclick="reject(' + "'" + myEvents[i].EVENTNAME + "'" + ')">Reject</button>' + "</td>" + "</tr>" + "</tr>";
+        tbody.innerHTML += tr;
+    }
 });
 
-$("#rejectbutton").click(function() {
-        var eventname = window.sessionStorage.getItem('eventname');
-        var firstname = $( "#firstname" ).val();
-        var lastname = $( "#lastname" ).val();
-        var registeremail = $( "#registeremail" ).val();
-        var gradyear = $( "#gradyear" ).val();
-        var guests = $( "#guests" ).val();
-        console.log(firstname + lastname + registeremail + gradyear + guests + eventname);
-});
-
-function approve(index) {
-    var eventname = myEvents[index].name;
-    window.sessionStorage.setItem('eventname', myEvents[index].name);
+function approve(eventname) {
+    alert(eventname + " has been approved");
 }
 
-function reject(index) {
-    var eventname = myEvents[index].name;
-    window.sessionStorage.setItem('eventname', myEvents[index].name);
+function reject(eventname) {
+    alert(eventname + " has been denied");
 }
--- admindash.js
+++ admindash.js
@@ -25,15 +25,15 @@ document.head.appendChild(imported);
 $(document).ready(function(){
     var myEvents = viewEvents();
     // Appending data to HTML table
-var tbody = document.getElementById('tbody');
-var title = document.getElementById('regTitle');
+    var tbody = document.getElementById('tbody');
+    var title = document.getElementById('regTitle');
 
-for (var i = 0; i < myEvents.length; i++) {
-    var tr = '<tr>';
-    tr += "<td>" + myEvents[i].EVENTDATE + "</td>" + "<td>" + myEvents[i].EVENTTIME + "</td>" + "<td class='eventname' value=" + "'" + myEvents[i].EVENTNAME + "'>" + myEvents[i].EVENTNAME + "</td>" + "<td>" + myEvents[i].EVENTDESCRIPTION + "</td>" + "<td>" + myEvents[i].EVENTLOCATION + "</td>" + "<td>" + myEvents[i].EVENTPOSTEDBY + "</td>" + "<td>" + '<button type="button" class="btn btn-info btn-sm" style="margin-top:30%;" id=' + i + ' onclick="deleteevent(' + i + ')">Delete</button>' + "</td>" + "</tr>";
-    tbody.innerHTML += tr;
-}
- });
+    for (var i = 0; i < myEvents.length; i++) {
+        var tr = '<tr>';
+        tr += "<td>" + myEvents[i].EVENTDATE + "</td>" + "<td>" + myEvents[i].EVENTTIME + "</td>" + "<td class='eventname' value=" + "'" + myEvents[i].EVENTNAME + "'>" + myEvents[i].EVENTNAME + "</td>" + "<td>" + myEvents[i].EVENTDESCRIPTION + "</td>" + "<td>" + myEvents[i].EVENTLOCATION + "</td>" + "<td>" + myEvents[i].EVENTPOSTEDBY + "</td>" + "<td>" + '<button type="button" class="btn btn-info btn-sm" style="margin-top:30%;" id=' + i + ' onclick="deleteevent(' + i + ')">Delete</button>' + "</td>" + "</tr>";
+        tbody.innerHTML += tr;
+    }
+});
 
 function deleteevent(index) {
     alert("Event " + index + " has been deleted");
